e8c81f098bf3993e1cfdaf50aff973d628071dd7
Madyan Eka <madyan.eka95@gmail.com>
2018-10-20 07:25:28 +0700

    refactor actions

--- src/store/store.js
+++ src/store/store.js
@@ -2,7 +2,7 @@ import Vue from 'vue'
 import Vuex from 'vuex'
 import counter from './modules/counter'
 
-import * as actions from './actions'
+import actions from './actions'
 import * as mutations from './mutations'
 import * as getters from './getters'
 
